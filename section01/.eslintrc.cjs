e4f10287e90361303fce8dc4f8f55fbfa907fa2e
csh <tmdgjs7495@naver.com>
2024-03-18 21:21:40 +0900

    props, state 기본 예제

--- section01/.eslintrc.cjs
+++ section01/.eslintrc.cjs
@@ -18,6 +18,7 @@ module.exports = {
       { allowConstantExport: true },
     ],
     "no-unused-vars":"off",
-    "react/prop-types": "off"
+    "react/prop-types": "off",
+    "react/react-in-jsx-scope": "off"
   },
 }
--- section01/src/App.jsx
+++ section01/src/App.jsx
@@ -1,9 +1,16 @@
-function App() {
+import React from 'react'
+import Bulb from "./components/Bulb"
+import Counter from './components/Counter'
 
-  return (
-    <>
-    </>
-  )
+function App() {
+  // 자식 요소가 re-rendering 된다고 해서 부모 요소도 re-rendering 되는 것은 아니다.
+  console.log("rendering")
+ return (
+  <>
+    <Bulb/>
+    <Counter/>
+  </>
+ )
 }
 
 export default App
